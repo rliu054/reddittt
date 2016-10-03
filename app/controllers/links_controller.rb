ab2f921b6647d87a44ffed10abb45ff0e2631556
Rui <iamliurui@gmail.com>
2016-10-03 15:49:58 -0400

    Tweak links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,6 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except: [:index, :show]
 
   # GET /links
   # GET /links.json
@@ -14,7 +15,9 @@ class LinksController < ApplicationController
 
   # GET /links/new
   def new
-    @link = Link.new
+    # @link = Link.new
+    # So that links are associated with a user
+    @link = current_user.links.build
   end
 
   # GET /links/1/edit
@@ -24,7 +27,7 @@ class LinksController < ApplicationController
   # POST /links
   # POST /links.json
   def create
-    @link = Link.new(link_params)
+    @link = current_user.links.build(link_params)
 
     respond_to do |format|
       if @link.save
